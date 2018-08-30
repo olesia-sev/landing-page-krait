aaa1bc00b2a0fe48970dfdf7cf8b0db49e1bb0f2
olesia-sev <olesya0191@gmail.com>
2018-08-30 08:26:26 +0300

    fix styles and markup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 
     //Плавный переход к секции по клику на пункт меню
     $(function () {
-        $('.menu__link, .menu__button-download, .burger-menu__link').on('click', function (e) {
+        $('.js-header-link, .js-btn-download, .js-burger-menu-link').on('click', function (e) {
             e.preventDefault();
 
             var $el = $(this),
@@ -14,59 +14,45 @@
             }, 600);
 
             //закрыть полноэкранное меню
-            $('.full-screen-menu').removeClass('visible');
-            $('.burger').removeClass('menu-on');
+            $('.js-full-screen-menu').removeClass('visible');
+            $('.js-burger').removeClass('menu-on');
             $('html, body').removeClass("no-scroll");
         });
 
         //иконка бургера по клику
-        $('.burger').on('click', function (e) {
+        $('.js-burger').on('click', function (e) {
             e.preventDefault();
-
             $(this).toggleClass('menu-on');
 
             //full-screen menu
-            $('.full-screen-menu').toggleClass('visible');
+            $('.js-full-screen-menu').toggleClass('visible');
             $('html, body').toggleClass("no-scroll");
         });
 
-        //слайдер со скриншотами
-/*        $('.carousel').carousel({
-            dist: -120,
-            shift: -80,
-            numVisible: 7
-        });
-
-        $(".js-carousel__button--right").on("click", function () {
-            $(".carousel").carousel('next');
-        });
-
-        $(".js-carousel__button--left").on("click", function () {
-            $(".carousel").carousel('prev');
-        });*/
-
         //video, play button
-        $(".video__play").on("click", function () {
+        var playButton = $('.js-video-play'),
+            film = $('.js-video-film'),
+            overlay = $('.js-video-overlay');
+
+        playButton.on("click", function () {
             if (video.paused === true) {
-                // Play the video
                 video.play();
-                $(".video__play").addClass('display_none');
-                $(".video__overlay").addClass('display_none');
-                $(".video__film").attr("controls", "controls");
+                playButton.addClass('display_none');
+                overlay.addClass('display_none');
+                film.attr("controls", "controls");
             } else {
-                // Pause the video
                 video.pause();
             }
         });
 
-        $(".video__film").on("click", function () {
+        film.on("click", function () {
             video.pause();
-            $(".video__play").removeClass('display_none');
-            $(".video__film").removeAttr("controls");
+            playButton.removeClass('display_none');
+            film.removeAttr("controls");
         });
 
         //слайдер с отзывами
-        $('.owl-carousel').owlCarousel({
+        $('.js-owl-carousel').owlCarousel({
             loop: true,
             margin: 30,
             dots: false,
@@ -84,11 +70,11 @@
         });
 
         $(".js-slider__button--right").on("click", function () {
-            $(".owl-carousel").trigger("next.owl.carousel");
+            $(".js-owl-carousel").trigger("next.owl.carousel");
         });
 
         $(".js-slider__button--left").on("click", function () {
-            $(".owl-carousel").trigger("prev.owl.carousel");
+            $(".js-owl-carousel").trigger("prev.owl.carousel");
         });
     });
 })(jQuery, video);
